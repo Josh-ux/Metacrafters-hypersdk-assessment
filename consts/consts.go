8f53e40d2b10d64747ad485242fed878027ed1ff
Josh-ux <52693978+Josh-ux@users.noreply.github.com>
2024-06-09 12:36:33 +0100

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Josh"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Kin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "jokin"
 )
 
 var ID ids.ID
